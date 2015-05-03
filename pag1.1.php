0b22c26fad4ee619ddfd10154efe983b462e6b34
raduCatalin <radu.catalin81@gmail.com>
2015-05-03 19:17:41 +0200

    30.05.15

--- pag1.1.php
+++ pag1.1.php
@@ -2,53 +2,41 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
 <html>
 	<head>
-	 <link rel="stylesheet" type="text/css" href="css/style.css">
-	 <script LANGUAGE="javascript">										//code dans le fichier HTML
-		 function afficherMessage (message) {
-			 if(document.getElementById)
-			document.getElementById('galerie').innerHTML = message;;
-			
-		}
-	</script>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	
 	<body> 
-	<div id="bandeau">	
-		<div id="titre"> NEW FILM FR</div>	
-	</div>
+		<div id="bandeau">	
+			<div id="titre"> NEW FILM FR</div>	
+		</div>
 	
 		<div id="p">
 		</div>
 		
-			<form id="recherche" action="" method="post">
-			     <label for="recherche-texte"><input id="recherche-texte" name="q" placeholder="recherche" value="" title="recherche" type="text" onfocus="if(value=='recherche') this.value='';" /></label>
-				 <input value="OK" name="recherche-submit" id="recherche-submit" type="submit" class="awesome" />
-			</form>
-	
 		<div id="menu">
 			<ul id="nav"><!--
 			--><li><a href="pag1.1.php">Accueil</a></li><!--
 			--><li><a href="pageInscription.php">Inscription</a></li><!--
-			--><li><a href="#">A propos</a></li><!--
-			--><li><a href="#">Contact</a></li><!--
+			--><li><a href="aPropos.html">A propos</a></li><!--
 			--><li><a href="connexion.php">Connexion</a></li>
 			</ul>
 		</div>
-		
 			
-				<div id="galerie">
-				<center>
+		<div id="galerie">
+			<center>
 				<?php
 					include "connect.inc.php";	
 					$images=mysql_query("select id_film, titre_film from film where year(date_sortie_film)=2014 ORDER BY `date_sortie_film` DESC limit 20");
-					while ($row = mysql_fetch_array($images)){ //src'?>
-						<span class="tit">TESE</span>
-						<IMG style=" width:160px; height:221px;" src=" <?php echo  'images/'.$row['id_film'].'.jpg'?>" onMouseOver="<?phpecho $row['titre_film'];?>">
+					while ($row = mysql_fetch_array($images)){?>
 						
+					<div class="item">
+						<img src=" <?php echo  'images/'.$row['id_film'].'.jpg'?>"  style=" width:160px; height:221px;" alt=""/>
+						<span class="caption"><?php echo  $row['titre_film']?></span>
+					</div>	
 				<?php } ?>
-				</center>
-				</div>	
+			</center>
+		</div>	
 			
-			<div id="piedpage"> lien 2 </div>
+		<div id="piedpage"> </div>
 	</body>
 </html>
--- pageInscription.php
+++ pageInscription.php
@@ -10,19 +10,12 @@
 	</div>
 	
 		<div id="p">
-		</div>
-		
-			<form id="recherche" action="" method="post">
-			     <label for="recherche-texte"><input id="recherche-texte" name="q" placeholder="recherche" value="" title="recherche" type="text" onfocus="if(value=='recherche') this.value='';" /></label>
-				 <input value="OK" name="recherche-submit" id="recherche-submit" type="submit" class="awesome" />
-			</form>
-	
+		</div>	
 		<div id="menu">
 			<ul id="nav"><!--
 			--><li><a href="pag1.1.php">Accueil</a></li><!--
 			--><li><a href="pageInscription.php">Inscription</a></li><!--
 			--><li><a href="aPropos.html">A propos</a></li><!--
-			--><li><a href="#">Contact</a></li><!--
 			--><li><a href="connexion.php">Connexion</a></li>
 			</ul>
 		</div>
@@ -33,29 +26,33 @@
 					<div>
 					<p>
 						<label id="formulaire">Nom:</label>
-						<input type="text" value="" name="nom" id="identifiant"/>
+						<input type="text" value="" name="nom" placeholder="Exemple" id="identifiant"/>
 					</p>
 					<p>
 						<label id="formulaire">Prenom:</label>
-						<input type="test" name="prenom" id="identifiant"/>
+						<input type="test" name="prenom" placeholder="Exemple" id="identifiant"/>
 					</p>
 					<p>
 						<label id="formulaire">Mot de passe:</label>
-						<input type="password" name="MPD" id="identifiant"/>
+						<input type="password" name="MPD" placeholder="Exemple" id="identifiant"/>
 					</p>
 					<p>
 						<label id="formulaire">Confirmer mot de passe:</label>
-						<input type="password" name="repeteMPD" id="identifiant"/>
+						<input type="password" name="repeteMPD" placeholder="Exemple" id="identifiant"/>
+					</p>
+					<p>
+						<label id="formulaire">Pseudo:</label>
+						<input type="test" name="pseudo" placeholder="Exemple" id="identifiant"/>
 					</p>
 					<p>
 						<label id="formulaire">E-mail:</label>
-						<input type="test" value="" name="e-mail" id="identifiant"/>
+						<input type="e-mail" value="" name="e-mail" placeholder="exemple@exemple.com" id="identifiant"/>
 					</p>
-					<label id="formulaire">Sexe:</label>
+					<label id="formulaire">Je suis:</label>
 					<input type="radio" value="Homme" name="sexe" id=""/>
-					<label>Homme</label>
+					<label>Un homme</label>
 					<input type="radio" value="Femme" name="sexe" id=""/>
-					<label>Femme</label>
+					<label>Une femme</label>
 					<br></br>
 					<center>
 						<input id="toto" type="submit"  value="Envoyer" name="submit"/>
@@ -70,28 +67,32 @@
 			$_POST['MPD'] =htmlentities(trim($_POST['MPD']));				//recuperer la valeur
 			$_POST['repeteMPD']=htmlentities(trim($_POST['repeteMPD']));	//recuperer la valeur
 			$_POST['e-mail'] =htmlentities(trim($_POST['e-mail']));			//recuperer la valeur
+			$_POST['pseudo'] =htmlentities(trim($_POST['pseudo']));			//recuperer la valeur
 			if (isset($_POST['sexe'])) {
 				$_POST['sexe'] = htmlentities(trim($_POST['sexe']));		//recuperer la valeur
 			}
 				
-				if($_POST['nom']&&$_POST['prenom']&&$_POST['MPD']&&$_POST['repeteMPD']&&isset($_POST['sexe']))  //verifie si les champs sont tous completes
+				if($_POST['nom']&&$_POST['prenom']&&$_POST['MPD']&&$_POST['repeteMPD']&&$_POST['pseudo']&&$_POST['e-mail']&&isset($_POST['sexe']))  //verifie si les champs sont tous completes
 				{
 					if($_POST['MPD']==$_POST['repeteMPD'])  //verifie si le 2 mots de passes sont identique 
 					{
-						include "connect.inc.php"; 			// le fichier qui fait la connexion a la base de donnee
-						$reg=mysql_query("SELECT * FROM utilisateur WHERE email_utilisateur='".$_POST['e-mail']."'");  //verifi si l adresse mail existe deja
-						$rows=mysql_num_rows($reg);			//compte le nr de ligns dans ma variable
-						
-						
-						if($rows==0)						//on verifie si il y a une e-mail identic dans notre base
-						{
-							$query="INSERT INTO utilisateur (nom_utilisateur, prenom_utilisateur, email_utilisateur, civilite_utilisateur_, mot_de_passe_utilisateur) 
-									VALUES('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['e-mail']."','".$_POST['sexe']."',MD5('".$_POST['MPD']."'))";	//enregistre la requet
-									mysql_query( $query );							// envoier la requet
-									echo mysql_error();								// afficher si il y a des erreurs
-									echo "Inscription  enregistree";
-						} 
-						else echo "Ce pseudo n'est pas disponible";
+						if(filter_var($_POST['e-mail'], FILTER_VALIDATE_EMAIL)==true){	
+							include "connect.inc.php"; 			// le fichier qui fait la connexion a la base de donnee
+							$reg=mysql_query("SELECT * FROM utilisateur WHERE email_utilisateur='".$_POST['e-mail']."'");  //verifi si l adresse e-mail existe deja
+							$rows=mysql_num_rows($reg);			//compte le nr de ligns dans ma variable
+							
+							
+							if($rows==0)						//on verifie si il y a une e-mail identic dans notre base
+							{
+								$query="INSERT INTO utilisateur (nom_utilisateur, prenom_utilisateur, email_utilisateur, civilite_utilisateur_, mot_de_passe_utilisateur, pseudo_utilisateur) 
+										VALUES('".$_POST['nom']."','".$_POST['prenom']."','".$_POST['e-mail']."','".$_POST['sexe']."',MD5('".$_POST['MPD']."'),'".$_POST['pseudo']."')";	//enregistre la requet
+										mysql_query( $query );							// envoier la requet
+										echo mysql_error();								// afficher si il y a des erreurs
+										echo "Inscription  enregistree";
+							} 
+							else echo "Ce pseudo n'est pas disponible";
+						}
+						else echo "Veuliiez saisir une adresse mail valide";
 					}
 					else echo'Les deux mots des passes doivent etre identique';
 				}
@@ -102,6 +103,6 @@
 					</div>
 				</form>
 			</div>
-			<div id="piedpage"> lien 2 </div>
+			<div id="piedpage"></div>
 	</body>
 </html>
